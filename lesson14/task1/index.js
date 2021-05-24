09da647047ebc163be902ba4e9adc867b7c5ca0b
Roman Hrynko <romanhrynkoua@gmail.com>
2021-05-24 09:38:40 +0300

    lesson14

---
+++ lesson14/task1/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let message = 'Just learn it';
+
+export const sendMessage = name => {
+  const sender = 'Gromcode';
+
+  console.log(`${name}, ${message}! Your ${sender}`);
+};
+
+export function setMessage(text) {
+  message = text;
+  return message;
+}
