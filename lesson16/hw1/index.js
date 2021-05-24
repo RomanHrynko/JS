603ac473fd48ba84cb7aa3e63ac14a10be9e7e3e
Roman Hrynko <romanhrynkoua@gmail.com>
2021-05-24 18:28:34 +0300

    lesson16

---
+++ lesson16/hw1/index.js
@@ -0,0 +1,19 @@
+export function createArrayOfFunctions(num) {
+  if (num === undefined) {
+    return [];
+  }
+
+  const arr = [];
+
+  for (let i = 0; i < num; i += 1) {
+    arr[i] = function () {
+      return i;
+    };
+  }
+
+  return typeof num !== 'number' ? null : arr;
+}
+
+console.log(createArrayOfFunctions(5));
+console.log(createArrayOfFunctions('11'));
+console.log(createArrayOfFunctions());
---
+++ lesson16/hw2/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+
+/* Начальный код для этой задачи написан с плохими практиками, такими как использования var, 
+переопределение переменной и тд. Это сделано намерено чтобы разобрать работу var.
+Чтобы eslint не ругался на ошибки, для этой задачи он отключен аннотацией eslint-disable */
+
+var foo = 1;
+
+function bar() {
+  if (!foo) {
+    var foo = 10;
+  }
+
+  return foo;
+}
+
+var foo = bar();
+
+// =====================
+
+function bar() {
+  let foo = 1;
+  if (!foo) {
+    foo = 10;
+  }
+
+  return foo;
+}
+
+let foo = bar();
+console.log(foo);
+
+export default foo;
