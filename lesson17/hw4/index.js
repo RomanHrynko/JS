7a812fb73f3e4c294f858897c21b8cf10f0b5888
Roman Hrynko <romanhrynkoua@gmail.com>
2021-05-25 13:58:01 +0300

    lesson17

---
+++ lesson17/hw4/index.js
@@ -0,0 +1,16 @@
+export const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  setFullName(fullName) {
+    const array = fullName.split(' ');
+    this.firstName = array[0];
+    this.lastName = array[1];
+  },
+};
+
+console.log(user.getFullName());
+
+console.log(user.setFullName('Roman Hrynko'));
