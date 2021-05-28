c47f26f5ff50e57e2db3549d251b79908684114d
Roman Hrynko <romanhrynkoua@gmail.com>
2021-05-28 09:09:24 +0300

    lesson20

---
+++ lesson20/task2/index.js
@@ -0,0 +1,37 @@
+class Vehicle {
+  constructor(name, numberOfWheels) {
+    this.name = name;
+    this.numberOfWheels = numberOfWheels;
+  }
+
+  move() {
+    console.log(`${this.name} is moving`);
+  }
+
+  stop() {
+    console.log(`${this.name} stopped`);
+  }
+}
+
+class Ship extends Vehicle {
+  constructor(name, maxSpeed) {
+    super(name, false);
+    this.maxSpeed = maxSpeed;
+  }
+
+  move() {
+    console.log(`${this.name} lifting anchor up`);
+    super.move();
+  }
+  stop() {
+    super.stop();
+    console.log(`${this.name} lifting anchor down`);
+  }
+}
+
+const user = new Ship('Tom', 25);
+console.log(user.move());
+console.log(user.stop());
+
+const user1 = new Vehicle('Argo', false);
+console.log(user1.move());
