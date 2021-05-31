abf0a270128d749c6670c9a4b89e556b1f84e9c1
Roman Hrynko <romanhrynkoua@gmail.com>
2021-05-31 15:02:44 +0300

    lesson21

---
+++ lesson21/hw10/index.js
@@ -0,0 +1,16 @@
+export const manageClasses = () => {
+  const firstElement = document.querySelector('.one');
+  firstElement.classList.add('selected');
+
+  const twoElement = document.querySelector('.two');
+  twoElement.classList.remove('selected');
+
+  const thirdElement = document.querySelector('.three');
+  thirdElement.classList.toggle('three_done');
+
+  const fourElement = document.querySelector('.four');
+
+  if (fourElement.classList.contains('some-class')) {
+    fourElement.classList.add('another-class');
+  }
+};
