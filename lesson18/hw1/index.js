7387aefa81fee3a52a018129114dd64ee8b4bd12
Roman Hrynko <romanhrynkoua@gmail.com>
2021-05-31 18:49:57 +0300

    lesson18

--- lesson18/hw1/index.js
+++ lesson18/hw1/index.js
@@ -7,3 +7,25 @@ function saveCalls(func) {
 
   return withMemory;
 }
+
+function test(a, b) {
+  return Math.sqrt(a * a + b * b);
+}
+
+const testWithMemory = saveCalls(test);
+testWithMemory(4, 2);
+testWithMemory(9, 1);
+console.log(testWithMemory.calls);
+
+const user = {
+  name: 'John',
+  sayHi() {
+    return this.name;
+  },
+};
+
+const methodWithMemory = saveCalls(user.sayHi);
+
+console.log(methodWithMemory.apply({ name: 'Tom' })); // 'Tom'
+
+console.log(methodWithMemory.calls);
