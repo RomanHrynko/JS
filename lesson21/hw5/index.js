988d9a9dad0479cefd6c947900e6569f488556a1
Roman Hrynko <romanhrynkoua@gmail.com>
2021-05-31 13:49:37 +0300

    export+

--- lesson21/hw5/index.js
+++ lesson21/hw5/index.js
@@ -1,4 +1,4 @@
-const setButton = buttonText => {
+export const setButton = buttonText => {
   const bodyElement = document.querySelector('body');
   bodyElement.innerHTML = `<button>${buttonText}</button>`;
 };
