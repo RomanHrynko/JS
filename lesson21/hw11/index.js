aa6318ec8fe5944dda6f78aa0c366aaba70c1926
Roman Hrynko <romanhrynkoua@gmail.com>
2021-05-31 15:21:16 +0300

    lesson21

---
+++ lesson21/hw11/index.js
@@ -0,0 +1,8 @@
+const squaredNumbers = () => {
+  const numbersElement = document.querySelectorAll('.number');
+  console.log(numbersElement);
+  [...numbersElement].map(el => {
+    const num = el.dataset.number;
+    el.dataset.squaredNumber = num * num;
+  });
+};
