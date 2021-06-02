825e81b6585c8d648996219f2797ff6b5b1b36eb
Roman Hrynko <romanhrynkoua@gmail.com>
2021-06-02 14:09:56 +0300

    lesson24

---
+++ lesson24/task1/index.js
@@ -0,0 +1,14 @@
+const daysOfWeek = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
+
+export const dayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+  console.log(day);
+
+  const dateInFuture = new Date(date).setDate(day + days);
+  console.log(new Date(dateInFuture));
+
+  return daysOfWeek[new Date(dateInFuture).getDay()];
+};
+
+const result = dayOfWeek(new Date(2021, 5, 2), 30);
+console.log(result);
