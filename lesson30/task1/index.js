1ae9a523b36ed73961632ef6927f9b205cf955ef
Roman Hrynko <romanhrynkoua@gmail.com>
2021-06-08 14:35:07 +0300

    lesson30-rask1

---
+++ lesson30/task1/index.js
@@ -0,0 +1,28 @@
+export const addImage = imgSrc => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+    const onError = () => reject(new Error('Image load is failed...'));
+
+    imgElem.addEventListener('error', onError);
+  });
+
+  return p;
+};
+
+const img =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+const res = addImage(img);
+console.log(res);
